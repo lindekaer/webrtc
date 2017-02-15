4bb0ca64fcb242ca71e49cbabe94879081dbbb2d
Theodor Lindekaer <theodor.lindekaer@gmail.com>
2017-02-15 20:31:52 +0100

    Bacon

--- test.sh
+++ test.sh
@@ -3,24 +3,24 @@ signaling_server=$2
 
 number="5"
 for num in {1..5}
-  do node dist/runner.js --num-containers 2 --num-peers ${number} --mode full --signaling-url ${signaling_server} --output-file "test${env}_${number}.data"
+  do node dist/runner.js --num-containers 2 --num-peers ${number} --mode full --signaling-url ${signaling_server} --output-file "test_${env}_${number}.data"
   sleep 10
 done
 
 number="10"
 for num in {1..5}
-  do node dist/runner.js --num-containers 2 --num-peers ${number} --mode full --signaling-url ${signaling_server} --output-file "test${env}_${number}.data"
+  do node dist/runner.js --num-containers 2 --num-peers ${number} --mode full --signaling-url ${signaling_server} --output-file "test_${env}_${number}.data"
   sleep 10
 done
 
 number="20"
 for num in {1..5}
-  do node dist/runner.js --num-containers 2 --num-peers ${number} --mode full --signaling-url ${signaling_server} --output-file "test${env}_${number}.data"
+  do node dist/runner.js --num-containers 2 --num-peers ${number} --mode full --signaling-url ${signaling_server} --output-file "test_${env}_${number}.data"
   sleep 10
 done
 
 number="40"
 for num in {1..5}
-  do node dist/runner.js --num-containers 2 --num-peers ${number} --mode full --signaling-url ${signaling_server} --output-file "test${env}_${number}.data"
+  do node dist/runner.js --num-containers 2 --num-peers ${number} --mode full --signaling-url ${signaling_server} --output-file "test_${env}_${number}.data"
   sleep 10
 done
