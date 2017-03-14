e6898cc2b34f77ef69d0c64c20712ee76a6d0215
Niklas Stokkebro Gundlev <niklasgundlev@gmail.com>
2017-03-14 22:31:21 +0100

    Bacon

--- dist/config.js
+++ dist/config.js
@@ -16,8 +16,8 @@ exports.default = {
       OfferToReceiveVideo: false
     }
   },
-  // webSocketUrl: 'SIGNALING_URL',
-  // uuid: 'SIGNALING_UUID'
-  webSocketUrl: 'ws://192.168.1.242:8080/socketserver',
-  uuid: Math.random() > 0.5 ? 'meep' : 'beans'
+  webSocketUrl: 'SIGNALING_URL',
+  uuid: 'SIGNALING_UUID'
+  // webSocketUrl: 'ws://192.168.1.242:8080/socketserver',
+  // uuid: Math.random() > 0.5 ? 'meep' : 'beans'
 };
