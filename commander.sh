6bb0858276379b5c1e42e14a1f13e2815b50c737
Theodor Lindekaer <theodor.lindekaer@gmail.com>
2017-03-18 11:34:40 +0100

    Bacon

--- commander.sh
+++ commander.sh
@@ -7,13 +7,13 @@ read -d '' command << EOF
   cd /webrtc;
   git stash;
   git fetch --all;
-  git checkout experiment-dice; 
+  git checkout experiment-data-channel; 
   git stash;
   git pull;
   npm run build;
-  node dist/runner.js --num-containers 1 --num-peers 2 --mode spawn --signaling-url ws://188.226.135.47:8080/socketserver --timeout 100000000 --first-peer;
+  node dist/runner.js --num-containers 1 --num-peers 2 --mode spawn --signaling-url ws://174.138.65.125:8080/socketserver --timeout 100000000 --first-peer;
 EOF
-ssh root@192.241.153.93 -t ${command}
+ssh root@192.81.210.22 -t ${command}
 fi
 
 if [ $mode = "spawn2" ]
@@ -22,13 +22,13 @@ read -d '' command << EOF
   cd /webrtc;
   git stash;
   git fetch --all;
-  git checkout experiment-dice;
+  git checkout experiment-data-channel;
   git stash;
   git pull;
   npm run build;
-  node dist/runner.js --num-containers 1 --num-peers 2 --mode spawn --signaling-url ws://188.226.135.47:8080/socketserver --timeout 100000000;
+  node dist/runner.js --num-containers 1 --num-peers 2 --mode spawn --signaling-url ws://174.138.65.125:8080/socketserver --timeout 100000000;
 EOF
-ssh root@207.154.228.89 -t ${command}
+ssh root@207.154.192.25 -t ${command}
 fi
 
 if [ $mode = "walker" ]
@@ -37,22 +37,22 @@ read -d '' command << EOF
   cd /webrtc;
   git stash;
   git fetch --all;
-  git checkout experiment-dice;
+  git checkout experiment-data-channel;
   git stash;
   git pull;
   npm run build;
-  node dist/runner.js --num-containers 2 --num-peers 2 --mode walker --signaling-url ws://188.226.135.47:8080/socketserver --id experiment-1;
+  node dist/runner.js --num-containers 2 --num-peers 2 --mode walker --signaling-url ws://174.138.65.125:8080/socketserver --id experiment-1;
 EOF
-ssh root@46.101.81.163 -t ${command}
+ssh root@188.166.157.144 -t ${command}
 fi
 
 if [ $mode = "clean" ]
 then
-ssh root@192.241.153.93 -t 'docker stop $(docker ps -a -q)'
+ssh root@192.81.210.22 -t 'docker stop $(docker ps -a -q)'
 sleep 2
-ssh root@207.154.228.89 -t 'docker stop $(docker ps -a -q)'
+ssh root@207.154.192.25 -t 'docker stop $(docker ps -a -q)'
 sleep 2
-ssh root@46.101.81.163 -t 'docker stop $(docker ps -a -q)'
+ssh root@188.166.157.144 -t 'docker stop $(docker ps -a -q)'
 fi
 
 
