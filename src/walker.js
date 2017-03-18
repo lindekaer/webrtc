6d896152ba2e147e043977615e99d850c39d1011
Theodor Lindekaer <theodor.lindekaer@gmail.com>
2017-03-18 12:03:10 +0100

    Bacon

--- src/walker.js
+++ src/walker.js
@@ -36,6 +36,7 @@ class WalkerPeer {
   }
 
   connectToServer () {
+    console.log('Connected to server...')
     this._signalingChannel = new window.WebSocket(config.webSocketUrl)
     this._signalingChannel.onopen = this.onSocketOpen.bind(this)
     this._signalingChannel.onmessage = this.onSocketMessage.bind(this)
