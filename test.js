637dbd8c546f1b4b5f340b1342d913cce645d867
Theodor Lindekaer <theodor.lindekaer@gmail.com>
2017-03-20 14:48:13 +0100

    Bacon

--- test.js
+++ test.js
@@ -1,20 +1,6 @@
 
-var str = `
-file:///app/walker.html 2723:10 "1490003879597 - ##LOG## Connection established to node 5"
-file:///app/walker.html 2843:18 "Closing connection"
-file:///app/walker.html 2805:16 "On message"
-file:///app/walker.html 2723:10 "1490003879111 - ##LOG## Connection established to node 6"
-file:///app/walker.html 2803:14 "On data channel"
-file:///app/walker.html 2834:16 "On open"
+var str = `### LOG ###1490016298072,2,3`
 
-`
-
-var timestamps = []
-var output = str.split('\n')
-for (let i = 0; i < output.length; i++) {
-  if (output[i].indexOf('##LOG##') !== -1) {
-    timestamps.push(output[i])
-  }
-}
-
-console.log(timestamps)
+var index = str.indexOf('### LOG ###') + String('### LOG ###').length
+ 
+console.log(str.split('### LOG ###'))
