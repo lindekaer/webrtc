c8e6a54fcb722f58d54224b90a20644d6f4c9c02
Theodor Lindekaer <theodor.lindekaer@gmail.com>
2017-03-21 12:58:04 +0100

    Bacon

--- commander.sh
+++ commander.sh
@@ -4,13 +4,10 @@ mode=$1
 if [ $mode = "spawn1" ]
 then
 read -d '' command << EOF
-  cd /webrtc; 
-  git stash;
-  git pull;
-  npm run build;
-  node dist/runner.js --num-containers 2 --num-peers 10 --mode spawn --signaling-url ws://188.226.135.47:8080/socketserver --timeout 120000 --first-peer;
+  docker stop $(docker ps -a -q)
+  node dist/runner.js --num-containers 1 --num-peers 64 --mode spawn --signaling-url ws://174.138.65.125:8080/socketserver --first-peer;
 EOF
-ssh root@192.241.153.93 -t ${command}
+ssh root@192.81.210.22 -t ${command}
 fi
 
 if [ $mode = "spawn2" ]
